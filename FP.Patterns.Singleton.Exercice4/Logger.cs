3ddbbac0f2001ab3affcd197e03b1d977e40395a
Frank Palomino <frank.palomino1@gmail.com>
2024-06-06 16:15:23 +0200

    Exercice 4

---
+++ FP.Patterns.Singleton.Exercice4/Logger.cs
@@ -0,0 +1,35 @@
+﻿namespace FP.Patterns.Singleton.Exercice4
+{
+    public class Logger
+    {
+        private static Logger _logger;
+        private List<string> _logs;
+
+        private Logger() 
+        {
+            _logs = new List<string>();
+        }
+
+        public static Logger GetLogger()
+        {
+            if(_logger is null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+
+        public void Log(string message)
+        {
+            _logs.Add(message);
+        }
+
+        public void ShowLogs()
+        {
+            foreach(string log in _logs)
+            {
+                Console.WriteLine(log);
+            }
+        }
+    }
+}
---
+++ FP.Patterns.Singleton.Exercice4/Program.cs
@@ -0,0 +1,12 @@
+﻿using FP.Patterns.Singleton.Exercice4;
+
+Logger log = Logger.GetLogger();
+log.Log("First log");
+log.ShowLogs();
+
+Console.WriteLine("--------------------------");
+
+Logger log2 = Logger.GetLogger();
+log2.Log("Second log");
+
+log2.ShowLogs();
