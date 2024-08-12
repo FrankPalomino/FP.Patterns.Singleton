d10374f0fc21e721b5b3d6d9dbdf8ce93bcae977
Frank Palomino <frank.palomino1@gmail.com>
2024-08-12 19:08:40 +0200

    Exercice 5

---
+++ FP.Patterns.Singleton.Exercice5/Logger.cs
@@ -0,0 +1,22 @@
+﻿namespace FP.Patterns.Singleton.Exercice5
+{
+    public class Logger
+    {
+        private static Logger _logger;
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if(_logger is null)
+            {
+                _logger = new Logger();    
+            }
+            return _logger;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ FP.Patterns.Singleton.Exercice5/Program.cs
@@ -0,0 +1,9 @@
+﻿using FP.Patterns.Singleton.Exercice5;
+
+Logger log = Logger.GetInstance();
+
+log.Log("Firt Message");
+
+Logger log2 = Logger.GetInstance();
+
+log2.Log("Another message");
